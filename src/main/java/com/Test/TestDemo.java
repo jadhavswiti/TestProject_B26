52c25f74203a04509e6c18d2a26e264c44604106
jadhavswiti <jadhavswiti1@gmail.com>
2024-10-18 17:39:32 +0530

    added sample

--- src/main/java/com/Test/TestDemo.java
+++ src/main/java/com/Test/TestDemo.java
@@ -3,8 +3,9 @@ package com.Test;
 public class TestDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("Welcome To BugSpotter Institute");
+		
 	}
 
 }
---
+++ src/main/java/com/Test/sampletest.java
@@ -0,0 +1,12 @@
+package com.Test;
+
+public class sampletest {
+
+	public static void main(String[] args) {
+
+		//feature123
+		
+		System.out.println("Sample test");
+	}
+
+}
