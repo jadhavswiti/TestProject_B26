9438fd3e1cf56396b54d9656a8ade2c27669790f
jadhavswiti <jadhavswiti1@gmail.com>
2024-10-18 17:11:48 +0530

    added sample test

--- src/main/java/com/Test/DemoTest.java
+++ src/main/java/com/Test/DemoTest.java
@@ -1,10 +1,10 @@
 package com.Test;
 
+
 public class DemoTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+System.out.println("Today's Task completed");
+}
 
 }
